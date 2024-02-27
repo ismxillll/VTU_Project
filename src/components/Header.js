79088cbf892fce63b1b469c42d40b1e2590e1f83
Mohammed Ismail <mi607694@gmail.com>
2024-02-27 22:04:30 +0530

    Created Footer Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto">
+                        <Nav className="m-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
                             <Nav.Link href="#link">Link</Nav.Link>
                             <NavDropdown title="Acadamics" id="basic-nav-dropdown">
